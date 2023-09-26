b0510a40ade83b4246e2472798a39077d47e17d0
edupoly <info.edupoly@gmail.com>
2023-09-26 10:35:23 +0530

    sdf

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Counter from './Counter';
+import './App.css';
+import Todolist from './Todolist';
+import { Outlet,Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='mybox'>
+      <h1>ReactJS Application by Praveen Gubbala</h1>
+      <Link to="/counter">Counter</Link>&nbsp;&nbsp;
+      <Link to="/todolist">Todolist</Link>&nbsp;&nbsp;
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Counter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Todolist from './Todolist'
+function Counter(){
+    return (
+        <div className='mybox'>
+            <h1>Welcome to Counter</h1>
+            
+        </div>
+    )
+}
+export default Counter;
