c8c43475db595f559511f2adc5b87ca85179f9d8
edupoly <info.edupoly@gmail.com>
2023-10-06 10:57:19 +0530

    xcsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Counter from './features/counter/Counter';
+import Todolist from './features/todolist/Todolist';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="mybox">
+      <h1>App</h1>
+      <Todolist></Todolist>
+      <Counter></Counter>
     </div>
   );
 }
